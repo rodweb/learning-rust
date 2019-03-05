350d9d7ef599d75d49bc39050cc3c9c8b1f02bd1
Rodrigo Campos <rod.apd@gmail.com>
2019-03-05 13:02:13 -0300

    enums

--- data_structures/src/main.rs
+++ data_structures/src/main.rs
@@ -20,6 +20,31 @@ fn structures() {
     let line = Line{ start: p, end: p2 };
 }
 
+enum Color {
+    Red,
+    Green,
+    Blue,
+    RgbColor(u8,u8,u8), // tuple
+    CmykColor{cyan:u8, magenta:u8, yellow:u8, black: u8}, // struct
+}
+
+fn enums() {
+    //let c:Color = Color::Red;
+    //let c:Color = Color::RgbColor(10,0,0);
+    let c:Color = Color::CmykColor{cyan: 0, magenta: 0, yellow: 0, black: 255};
+
+    match c {
+        Color::Red => println!("r"),
+        Color::Green => println!("g"),
+        Color::Blue => println!("b"),
+        Color::RgbColor(0,0,0)
+        | Color::CmykColor{cyan:_,magenta:_,yellow:_,black:255} => println!("black"),
+        Color::RgbColor(r,g,b) => println!("rgb({},{},{})", r,g,b),
+        _ => ()
+    }
+}
+
 fn main() {
     structures();
+    enums();
 }
