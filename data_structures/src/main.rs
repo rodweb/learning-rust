955c5679b0276b14a6d3456c3df5a9f810012f95
Rodrigo Campos <rod.apd@gmail.com>
2019-03-05 12:54:08 -0300

    structs

---
+++ data_structures/src/main.rs
@@ -0,0 +1,25 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+struct Point {
+    x:f64,
+    y:f64,
+}
+
+struct Line {
+    start: Point,
+    end: Point
+}
+
+fn structures() {
+    let p = Point{ x: 3.0, y: 4.0 };
+    println!("point p is at ({}, {})", p.x, p.y);
+
+    let p2 = Point{ x: 5.0, y: 10.0 };
+
+    let line = Line{ start: p, end: p2 };
+}
+
+fn main() {
+    structures();
+}
