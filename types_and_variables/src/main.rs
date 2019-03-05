f8bc9b8e2f6192c97b4a7a8ef25c1fab0e182dea
Rodrigo Campos <rod.apd@gmail.com>
2019-03-05 10:54:02 -0300

    operators

--- types_and_variables/src/main.rs
+++ types_and_variables/src/main.rs
@@ -1,6 +1,41 @@
 use std::mem;
 
-fn main() {
+fn operators() {
+    // arithmetic
+    let mut a = 2+3*4;
+    println!("{}", a);
+    a += 2;
+    println!("{}", a);
+
+    println!("remainder of {} / {} = {}", a, 3, (a%3));
+
+    let a_cubed = i32::pow(a, 3);
+    println!("{} cubed is {}", a, a_cubed);
+
+    let b = 2.5;
+    let b_cubed = f64::powi(b, 3);
+    let b_to_pi = f64::powf(b, std::f64::consts::PI);
+    println!("{} cubed = {}, {}^pi = {}", b, b_cubed, b, b_to_pi);
+
+    // bitwise
+    let c = 1 | 2; // | OR & AND ^ XOR ! NOR
+                   // 01 OR 10 = 11 == 3_10
+    println!("1|2 = {}", c);
+
+    let two_to_10 = 1 << 10; // 1    0   0   0   0  0  0  0 0 0
+                             // 1024 512 256 128 64 32 16 8 4 2
+    println!("2^10 = {}", two_to_10);
+
+    // logical
+    let pi_less_4 = std::f64::consts::PI < 4.0; // true
+    println!("pi is less than 4? {}", pi_less_4);
+
+    let x = 5;
+    let is_x_5 = x == 5;
+    println!("is x 5? {}", is_x_5);
+}
+
+fn declarations() {
     // immutable unsigned 0..255
     let a:u8 = 123; //8bits
     println!("a = {}", a);
@@ -33,4 +68,10 @@ fn main() {
     // true false
     let g = false;
     println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
+}
+
+
+fn main() {
+    declarations();
+    operators();
 }
